1c8184f1a594efef9f0ebd66f079b1be1fd552bc
kirilivanov <k.ivanov@loyalgarden.co>
2019-10-25 18:17:52 +0200

    Rinkeby ready

--- truffle-config.js
+++ truffle-config.js
@@ -71,7 +71,7 @@ module.exports = {
             provider: () => new HDWalletProvider(mnemonicRinkeby, `https://rinkeby.infura.io/v3/${infuraKey}`),
             network_id: 4,       // Rinkeby's id
             gas: 6550000,
-            confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+            confirmations: 0,    // # of confs to wait between deployments. (default: 0)
             timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
             skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
         },
@@ -79,7 +79,7 @@ module.exports = {
             provider: () => new HDWalletProvider(mnemonicRinkeby, `https://ropsten.infura.io/v3/${infuraKey}`),
             network_id: 3,
             gas: 6550000,
-            confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+            confirmations: 0,    // # of confs to wait between deployments. (default: 0)
             timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
             skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
         },
